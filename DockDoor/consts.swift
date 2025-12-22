d611030622ae5ff9d6917916cfa292b30b1a401f
Mohamad Yahia <xyzt70@gmail.com>
2025-12-22 05:27:47 +0400

    Update consts.swift

--- DockDoor/consts.swift
+++ DockDoor/consts.swift
@@ -229,10 +229,6 @@ extension Defaults.Keys {
 
     static let alternateKeybindKey = Key<UInt16>("alternateKeybindKey", default: 0)
     static let alternateKeybindMode = Key<SwitcherInvocationMode>("alternateKeybindMode", default: .activeAppOnly)
-
-    // MARK: - Window Order Persistence
-
-    static let persistedWindowOrder = Key<[WindowOrderPersistence.PersistedWindowEntry]>("persistedWindowOrder", default: [])
 }
 
 // MARK: Display Configurations
